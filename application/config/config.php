39b88845c556ac9cbdfc08229495a6a57b2a44c7
Ludger <ludger@richard.rdla@gmail.com>
2019-06-09 17:25:08 +0200

    V4

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.20.110.223/';
+$config['base_url'] = 'http://51.68.230.246/';
 
 /*
 |--------------------------------------------------------------------------
